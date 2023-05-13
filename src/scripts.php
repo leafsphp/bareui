4ff8c96da8f2b68d9c36ea5042e2ac717e6cc9dc
mychidarko <mickdd22@gmail.com>
2023-05-13 08:40:54 +0000

    fix: update script

--- src/scripts.php
+++ src/scripts.php
@@ -1,5 +1,7 @@
 <?php
 
 if (class_exists('\Leaf\App')) {
-    \Leaf\View::attach(\Leaf\BareUI::class, 'template');
+    app()->attach(function () {
+        \Leaf\View::attach(\Leaf\BareUI::class, 'template');
+    });
 }

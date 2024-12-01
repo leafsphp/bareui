40e6483710e889e624150057ca479f65a191a19c
mychidarko <mickdd22@gmail.com>
2024-12-01 17:40:40 +0000

    fix: patch up leaf view issue

--- src/scripts.php
+++ src/scripts.php
@@ -2,6 +2,6 @@
 
 if (class_exists('\Leaf\Config')) {
     \Leaf\Config::addScript(function () {
-        \Leaf\View::attach(\Leaf\BareUI::class, 'template');
+        \Leaf\App::attachView(\Leaf\BareUI::class, 'template');
     });
 }
